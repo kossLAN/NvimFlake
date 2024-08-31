850ef8aeb53cc173c3a7f821235d7f4c77799ce8
kossLAN <kosslan@kosslan.dev>
2024-08-31 15:15:07 -0400

    whichkey: fix annoying error

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -1,13 +1,14 @@
-local whichkey = require('which-key')
+require('which-key').setup()
+-- local whichkey = require('which-key')
+--
+-- whichkey.setup()
 
-whichkey.setup()
-
-whichkey.add {
-  { '<leader>c', group = '[C]ode' },
-  { '<leader>d', group = '[D]ocument' },
-  { '<leader>r', group = '[R]ename' },
-  { '<leader>s', group = '[S]earch' },
-  { '<leader>w', group = '[W]orkspace' },
-  { '<leader>t', group = '[T]oggle' },
-  { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-}
+-- whichkey.add {
+--   { '<leader>c', group = '[C]ode' },
+--   { '<leader>d', group = '[D]ocument' },
+--   { '<leader>r', group = '[R]ename' },
+--   { '<leader>s', group = '[S]earch' },
+--   { '<leader>w', group = '[W]orkspace' },
+--   { '<leader>t', group = '[T]oggle' },
+--   { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+-- }
